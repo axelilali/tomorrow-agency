cdb385491d32dc3d0f15310cb8827ca303124ac3
Axel Ilali <axel.ilali@yahoo.fr>
2023-02-27 00:59:13 +0100

    feat: add pulse animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ jQuery(function ($) {
   function handleMouseMove(e) {
     const x = e.clientX - this.offsetLeft - this.offsetWidth / 2
     const y = e.clientY - this.offsetTop - this.offsetHeight / 2
-    this.style.transform = `rotateX(${x / 150}deg) rotateY(${y / 150}deg)`
+    this.style.transform = `rotateX(${x / 100}deg) rotateY(${y / 100}deg)`
   }
 
   // scale animation
@@ -52,13 +52,20 @@ jQuery(function ($) {
     $('#home-hero .slider').slick('slickPrev')
   })
 
-  const img = document.querySelector('.scaling-img')
-  const container = document.querySelector('.scaling-container')
+  const images = document.querySelectorAll('.scaling-img')
+  const container = document.querySelectorAll('.scaling-container')
 
   window.addEventListener('scroll', function () {
     const scrollTop = window.pageYOffset
-    const scaleFactor = 1 + scrollTop / 20000 /* adjust the scaling factor as desired */
-    img.style.transform = `scale(${scaleFactor})`
+    const scaleFactor = 1 + scrollTop / 15000 /* adjust the scaling factor as desired */
+
+    images.forEach((image) => {
+      console.log(image.getBoundingClientRect().top)
+      // if(image.getBoundingClientRect().top < 500) {
+        image.style.transform = `scale(${scaleFactor})`
+      // }
+      
+    })
   })
 
   ScrollReveal().reveal('section .small-title, section .section-title', {
